686bff981d326ed2df6576e04f812c09680b0dc0
roluminox <roluminox@gmail.com>
2025-07-08 22:07:09 +0200

    feat(server): add initial OPC UA server exposing 'temperature' variable

--- src/opc-ua-project.cpp
+++ src/opc-ua-project.cpp
@@ -1,20 +1,28 @@
-// opc-ua-project.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+#include <open62541/plugin/log_stdout.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    UA_Server* server = UA_Server_new();
+    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+
+    // Exemple : exposer une variable "temperature"
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    UA_Double temp = 42.0;
+    UA_Variant_setScalar(&attr.value, &temp, &UA_TYPES[UA_TYPES_DOUBLE]);
+    UA_NodeId tempNodeId = UA_NODEID_STRING(1, (char*)"temperature");
+    UA_QualifiedName tempName = UA_QUALIFIEDNAME(1, (char*)"temperature");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId variableType = UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE);
+
+    UA_Server_addVariableNode(server, tempNodeId, parentNodeId,
+        UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+        tempName, variableType, attr, NULL, NULL);
+
+    volatile UA_Boolean running = UA_TRUE;
+    UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
